c5f472e01497cdd7f428bc2141c13e3ce750c3e0
Harel Tussi <hareltussi@Works-MacBook-Pro.local>
2022-10-02 12:55:33 +0300

    167-two-sum-ii-input-array-is-sorted

---
+++ medium/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  let p1 = 0;
+  let p2 = numbers.length;
+  while (p1 < p2) {
+    if (numbers[p1] + numbers[p2] == target) return [p1 + 1, p2 + 1];
+    else if (numbers[p1] + numbers[p2] < target) p1++;
+    else p2--;
+  }
+};
